02b554e29e7851ceb4d09f7a5bd8932f5572e27c
elanfraga <elanfraga@gmail.com>
2018-09-03 22:59:39 -0300

    Atualização de usuário com PUT

--- src/main/java/com/elan/workshopmongo/resources/UserRecource.java
+++ src/main/java/com/elan/workshopmongo/resources/UserRecource.java
@@ -44,6 +44,14 @@ public class UserRecource {
         return ResponseEntity.created(uri).build();
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id){
+        User obj = service.fromDTO(objDto);
+        obj.setId(id);
+        obj = service.update(obj);
+        return ResponseEntity.noContent().build();
+    }
+
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable String id){
         service.delete(id);
