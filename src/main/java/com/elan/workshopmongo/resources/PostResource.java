8ebcf234f3e236849fb40ec29d78c929896fe900
elanfraga <elanfraga@gmail.com>
2018-09-04 22:35:14 -0300

    Consulta simples com query methods

--- src/main/java/com/elan/workshopmongo/resources/PostResource.java
+++ src/main/java/com/elan/workshopmongo/resources/PostResource.java
@@ -1,13 +1,13 @@
 package com.elan.workshopmongo.resources;
 
+import com.elan.workshopmongo.repository.util.URL;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.elan.workshopmongo.domain.Post;
 import com.elan.workshopmongo.services.PostService;
+
+import java.util.List;
 
 
 @RestController
@@ -15,9 +15,19 @@ import com.elan.workshopmongo.services.PostService;
 public class PostResource {
     @Autowired
     private PostService service;
+
+
     @RequestMapping(value="/{id}", method=RequestMethod.GET)
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping("/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 }
